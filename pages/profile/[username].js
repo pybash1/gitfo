73f880306c1f08e959dd5b42259c3720a8a0c21c
pybash1 <example@example.com>
2022-06-09 18:22:25 +0530

    Fix Class Names

--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -39,114 +39,114 @@ export default function User() {
             <Head>
             <title>GitFo - Profile - {data?.login}</title>
             </Head>
-            <div class="fixed inset-x-0 bottom-0 px-4 pb-3">
-                <div class="relative px-4 py-3 text-white bg-blue-600 rounded-lg pr-14">
-                    <p class="text-sm font-medium text-left sm:text-center">
+            <div className="fixed inset-x-0 bottom-0 px-4 pb-3">
+                <div className="relative px-4 py-3 text-white bg-blue-600 rounded-lg pr-14">
+                    <p className="text-sm font-medium text-left sm:text-center">
                     Loved the Profile? Then you will be glad to know that {data?.name} is looking for jobs!
                     {" "}
-                    <a class="underline" href={`https://github.com/${data?.login}`}>Hire &rarr; </a>
+                    <a className="underline" href={`https://github.com/${data?.login}`}>Hire &rarr; </a>
                     </p>
                 </div>
             </div>
             <p
-                class="relative block p-8 overflow-hidden border border-gray-100 dark:border-none rounded-lg bg-gray-900"
+                className="relative block p-8 overflow-hidden border border-gray-100 dark:border-none rounded-lg bg-gray-900"
                 >
                 <span
-                    class="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+                    className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
                 ></span>
 
-                <div class="justify-between sm:flex">
+                <div className="justify-between sm:flex">
                     <div>
                     <a href={`https://github.com/${data?.login}`}>    
-                        <h5 class="text-xl font-bold text-gray-900 dark:text-white">
+                        <h5 className="text-xl font-bold text-gray-900 dark:text-white">
                             {data?.name}
                         </h5>
                     </a>
-                    <p class="mt-1 text-xs font-medium text-gray-600 dark:text-gray-300">AKA {data?.login}</p>
+                    <p className="mt-1 text-xs font-medium text-gray-600 dark:text-gray-300">AKA {data?.login}</p>
                     </div>
 
-                    <div class="flex-shrink-0 hidden ml-3 sm:block">
+                    <div className="flex-shrink-0 hidden ml-3 sm:block">
                     <img
-                        class="object-cover w-16 h-16 rounded-lg shadow-sm"
+                        className="object-cover w-16 h-16 rounded-lg shadow-sm"
                         src={data?.avatar_url}
                         alt=""
                     />
                     </div>
                 </div>
 
-                <div class="mt-4 sm:pr-8">
-                    <p class="text-sm text-gray-500 dark:text-gray-300">
+                <div className="mt-4 sm:pr-8">
+                    <p className="text-sm text-gray-500 dark:text-gray-300">
                     {data?.bio}
                     </p>
                 </div>
 
-                <dl class="flex mt-6">
-                    <div class="flex flex-col-reverse">
-                        <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Joined</dt>
-                        <dd class="text-xs text-gray-500 dark:text-gray-300">{data?.created_at.split("T")[0].split("-").reverse().join("/")}</dd>
+                <dl className="flex mt-6">
+                    <div className="flex flex-col-reverse">
+                        <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Joined</dt>
+                        <dd className="text-xs text-gray-500 dark:text-gray-300">{data?.created_at.split("T")[0].split("-").reverse().join("/")}</dd>
                     </div>
 
-                    <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                        <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Followers</dt>
-                        <dd class="text-xs text-gray-50 dark:text-gray-300">{data?.followers}</dd>
+                    <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                        <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Followers</dt>
+                        <dd className="text-xs text-gray-50 dark:text-gray-300">{data?.followers}</dd>
                     </div>
 
-                    <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                        <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Following</dt>
-                        <dd class="text-xs text-gray-50 dark:text-gray-300">{data?.following}</dd>
+                    <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                        <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Following</dt>
+                        <dd className="text-xs text-gray-50 dark:text-gray-300">{data?.following}</dd>
                     </div>
 
                     {data?.company ? (
-                        <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                            <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Currently Working For</dt>
-                            <dd class="text-xs text-gray-50 dark:text-gray-300">{data?.company}</dd>
+                        <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                            <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Currently Working For</dt>
+                            <dd className="text-xs text-gray-50 dark:text-gray-300">{data?.company}</dd>
                         </div>
                     ) : null}
 
                     {data?.location ? (
-                        <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                            <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Based in</dt>
-                            <dd class="text-xs text-gray-50 dark:text-gray-300">{data?.location}</dd>
+                        <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                            <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Based in</dt>
+                            <dd className="text-xs text-gray-50 dark:text-gray-300">{data?.location}</dd>
                         </div>
                     ) : null}
 
                     <Link href={`/repos/${data?.login}`}>
                         <a>
-                            <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                                <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Repositories</dt>
-                                <dd class="text-xs text-gray-50 dark:text-gray-300">{data?.public_repos}</dd>
+                            <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                                <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Repositories</dt>
+                                <dd className="text-xs text-gray-50 dark:text-gray-300">{data?.public_repos}</dd>
                             </div>    
                         </a>
                     </Link>
 
-                    <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                        <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Gists</dt>
-                        <dd class="text-xs text-gray-50 dark:text-gray-300">{data?.public_gists}</dd>
+                    <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                        <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Gists</dt>
+                        <dd className="text-xs text-gray-50 dark:text-gray-300">{data?.public_gists}</dd>
                     </div>
                 </dl>
             </p>
             &nbsp;&nbsp;
             {!data?.twitter_username && !data?.blog ? null : (
-            <article class="p-4 bg-gray-900 rounded-xl">
-                <div class="flex items-center">
-                    <div class="ml-3">
-                    <h5 class="text-lg font-medium text-white">Socials</h5>
-                    <div class="flow-root">
+            <article className="p-4 bg-gray-900 rounded-xl">
+                <div className="flex items-center">
+                    <div className="ml-3">
+                    <h5 className="text-lg font-medium text-white">Socials</h5>
+                    <div className="flow-root">
                     </div>
                     </div>
                 </div>
 
-                <ul class="mt-4 space-y-2">
+                <ul className="mt-4 space-y-2">
                     {data?.twitter_username ? (
                         <li>
                             <a
                                 href={`https://twitter.com/${data?.twitter_username}`}
                                 target="_blank"
-                                class="block h-full p-4 border border-gray-700 rounded-lg hover:border-pink-600"
+                                className="block h-full p-4 border border-gray-700 rounded-lg hover:border-pink-600"
                             >
-                                <h5 class="font-medium text-white">{data?.twitter_username}</h5>
+                                <h5 className="font-medium text-white">{data?.twitter_username}</h5>
 
-                                <p class="mt-1 text-xs font-medium text-gray-300">
+                                <p className="mt-1 text-xs font-medium text-gray-300">
                                 {data?.name}&apos;s twitter!
                                 </p>
                             </a>
@@ -157,11 +157,11 @@ export default function User() {
                             <a
                                 href={data?.blog}
                                 target="_blank"
-                                class="block h-full p-4 border border-gray-700 rounded-lg hover:border-pink-600"
+                                className="block h-full p-4 border border-gray-700 rounded-lg hover:border-pink-600"
                             >
-                                <h5 class="font-medium text-white">Website</h5>
+                                <h5 className="font-medium text-white">Website</h5>
 
-                                <p class="mt-1 text-xs font-medium text-gray-300">
+                                <p className="mt-1 text-xs font-medium text-gray-300">
                                 {data?.name}&apos;s website!
                                 </p>
                             </a>
@@ -172,26 +172,26 @@ export default function User() {
             )}
             &nbsp;&nbsp;
             {orgdata?.length===0 ? null : (
-            <article class="p-4 bg-gray-900 rounded-xl">
-                <div class="flex items-center">
-                    <div class="ml-3">
-                    <h5 class="text-lg font-medium text-white">Organizations</h5>
-                    <div class="flow-root">
+            <article className="p-4 bg-gray-900 rounded-xl">
+                <div className="flex items-center">
+                    <div className="ml-3">
+                    <h5 className="text-lg font-medium text-white">Organizations</h5>
+                    <div className="flow-root">
                     </div>
                     </div>
                 </div>
 
-                <ul class="mt-4 space-y-2">
+                <ul className="mt-4 space-y-2">
                     {orgdata?.map(org => (
                         <li>
                             <a
                                 href={`https://github.com/${org.login}`}
                                 target="_blank"
-                                class="block h-full p-4 border border-gray-700 rounded-lg hover:border-pink-600"
+                                className="block h-full p-4 border border-gray-700 rounded-lg hover:border-pink-600"
                             >
-                                <h5 class="font-medium text-white">{org.login}</h5>
+                                <h5 className="font-medium text-white">{org.login}</h5>
 
-                                <p class="mt-1 text-xs font-medium text-gray-300">
+                                <p className="mt-1 text-xs font-medium text-gray-300">
                                 {org.description}
                                 </p>
                             </a>
--- pages/repos/[username].js
+++ pages/repos/[username].js
@@ -47,38 +47,38 @@ export default function User() {
             <Head>
             <title>GitFo - Repositories</title>
             </Head>
-            <section class="text-center">
-                <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 text-white">
-                    <ul class="grid grid-cols-2 gap-4 border-2 border-blue-600 rounded-xl lg:grid-cols-4">
-                        <li class="p-8">
-                            <p class="text-2xl font-extrabold text-blue-500">{forked.length}</p>
-                            <p class="mt-1 text-lg font-medium">Forked Repos</p>
+            <section className="text-center">
+                <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 text-white">
+                    <ul className="grid grid-cols-2 gap-4 border-2 border-blue-600 rounded-xl lg:grid-cols-4">
+                        <li className="p-8">
+                            <p className="text-2xl font-extrabold text-blue-500">{forked.length}</p>
+                            <p className="mt-1 text-lg font-medium">Forked Repos</p>
                         </li>
 
-                        <li class="p-8">
-                            <p class="text-2xl font-extrabold text-blue-500">{template.length}</p>
-                            <p class="mt-1 text-lg font-medium">Templates</p>
+                        <li className="p-8">
+                            <p className="text-2xl font-extrabold text-blue-500">{template.length}</p>
+                            <p className="mt-1 text-lg font-medium">Templates</p>
                         </li>
 
-                        <li class="p-8">
-                            <p class="text-2xl font-extrabold text-blue-500">{stars}</p>
-                            <p class="mt-1 text-lg font-medium">Stars</p>
+                        <li className="p-8">
+                            <p className="text-2xl font-extrabold text-blue-500">{stars}</p>
+                            <p className="mt-1 text-lg font-medium">Stars</p>
                         </li>
 
-                        <li class="p-8">
-                            <p class="text-2xl font-extrabold text-blue-500">{mostUsedLang}</p>
-                            <p class="mt-1 text-lg font-medium">Most Used Language</p>
+                        <li className="p-8">
+                            <p className="text-2xl font-extrabold text-blue-500">{mostUsedLang}</p>
+                            <p className="mt-1 text-lg font-medium">Most Used Language</p>
                         </li>
                     </ul>
                 </div>
             </section>
-            <section class="text-center">
-                <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 text-white">
-                    <ul class="grid grid-cols-2 gap-4 border-2 border-blue-600 rounded-xl lg:grid-cols-4">
+            <section className="text-center">
+                <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 text-white">
+                    <ul className="grid grid-cols-2 gap-4 border-2 border-blue-600 rounded-xl lg:grid-cols-4">
                         {langsU?.map(lang => (
-                            <li class="p-8">
-                                <p class="text-2xl font-extrabold text-blue-500">{langs.filter(item => item===lang).length}</p>
-                                <p class="mt-1 text-lg font-medium">{lang}</p>
+                            <li className="p-8">
+                                <p className="text-2xl font-extrabold text-blue-500">{langs.filter(item => item===lang).length}</p>
+                                <p className="mt-1 text-lg font-medium">{lang}</p>
                             </li>
                         ))}
                     </ul>
@@ -87,80 +87,80 @@ export default function User() {
             <div className="grid grid-cols-2 gap-8 p-10">
                 {data.map(repo => (
                     <p
-                        class="relative block p-8 overflow-hidden border border-gray-100 dark:border-none rounded-lg bg-gray-900"
+                        className="relative block p-8 overflow-hidden border border-gray-100 dark:border-none rounded-lg bg-gray-900"
                         >
                         <span
-                            class="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+                            className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
                         ></span>
 
-                        <div class="justify-between sm:flex">
+                        <div className="justify-between sm:flex">
                             <div>
                             <a href={repo?.homepage || `https://github.com/${repo?.full_name}`}>
-                                <h5 class="text-xl font-bold text-gray-900 dark:text-white">
+                                <h5 className="text-xl font-bold text-gray-900 dark:text-white">
                                     {repo?.name}
                                     &nbsp;&nbsp;
-                                    {repo?.fork ? <strong class="border text-green-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Fork</strong> : null}
-                                    {repo?.is_template ? <strong class="border text-blue-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Template</strong> : null}
-                                    {repo?.archived ? <strong class="border text-yellow-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Archived</strong> : null}
-                                    {repo?.disabled ? <strong class="border text-red-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Disabled</strong> : null}
-                                    {repo?.full_name.split("/")[0]===repo?.full_name.split("/")[1] ? <strong class="border text-pink-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Profile</strong> : null}
+                                    {repo?.fork ? <strong className="border text-green-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Fork</strong> : null}
+                                    {repo?.is_template ? <strong className="border text-blue-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Template</strong> : null}
+                                    {repo?.archived ? <strong className="border text-yellow-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Archived</strong> : null}
+                                    {repo?.disabled ? <strong className="border text-red-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Disabled</strong> : null}
+                                    {repo?.full_name.split("/")[0]===repo?.full_name.split("/")[1] ? <strong className="border text-pink-500 border-current uppercase px-5 py-0.5  rounded-full text-[10px] tracking-wide">Profile</strong> : null}
                                 </h5>
                             </a>
-                            <p class="mt-1 text-xs font-medium text-gray-600 dark:text-gray-300">AKA {repo?.full_name}</p>
+                            <p className="mt-1 text-xs font-medium text-gray-600 dark:text-gray-300">AKA {repo?.full_name}</p>
                             </div>
                         </div>
 
-                        <div class="mt-4 sm:pr-8">
-                            <p class="text-sm text-gray-500 dark:text-gray-300">
+                        <div className="mt-4 sm:pr-8">
+                            <p className="text-sm text-gray-500 dark:text-gray-300">
                             {repo?.description}
                             </p>
                         </div>
 
-                        <dl class="flex mt-6">
-                            <div class="flex flex-col-reverse">
-                                <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Created</dt>
-                                <dd class="text-xs text-gray-500 dark:text-gray-300">{repo?.created_at.split("T")[0].split("-").reverse().join("/")}</dd>
+                        <dl className="flex mt-6">
+                            <div className="flex flex-col-reverse">
+                                <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Created</dt>
+                                <dd className="text-xs text-gray-500 dark:text-gray-300">{repo?.created_at.split("T")[0].split("-").reverse().join("/")}</dd>
                             </div>
 
-                            <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                                <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Stars</dt>
-                                <dd class="text-xs text-gray-50 dark:text-gray-300">{repo?.stargazers_count}</dd>
+                            <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                                <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Stars</dt>
+                                <dd className="text-xs text-gray-50 dark:text-gray-300">{repo?.stargazers_count}</dd>
                             </div>
 
-                            <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                                <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Watchers</dt>
-                                <dd class="text-xs text-gray-50 dark:text-gray-300">{repo?.watchers_count}</dd>
+                            <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                                <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Watchers</dt>
+                                <dd className="text-xs text-gray-50 dark:text-gray-300">{repo?.watchers_count}</dd>
                             </div>
 
-                            <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                                <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Forks</dt>
-                                <dd class="text-xs text-gray-50 dark:text-gray-300">{repo?.forks_count}</dd>
+                            <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                                <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Forks</dt>
+                                <dd className="text-xs text-gray-50 dark:text-gray-300">{repo?.forks_count}</dd>
                             </div>
 
                             {repo?.language ? (
-                                <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                                    <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Language</dt>
-                                    <dd class="text-xs text-gray-50 dark:text-gray-300">{repo?.language}</dd>
+                                <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                                    <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Language</dt>
+                                    <dd className="text-xs text-gray-50 dark:text-gray-300">{repo?.language}</dd>
                                 </div>
                             ) : null}
 
                             {repo?.location ? (
-                                <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                                    <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Based in</dt>
-                                    <dd class="text-xs text-gray-50 dark:text-gray-300">{repo?.location}</dd>
+                                <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                                    <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Based in</dt>
+                                    <dd className="text-xs text-gray-50 dark:text-gray-300">{repo?.location}</dd>
                                 </div>
                             ) : null}
 
                             {repo?.license ? (
-                                <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                                    <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">License</dt>
-                                    <dd class="text-xs text-gray-50 dark:text-gray-300">{repo?.license?.name}</dd>
+                                <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                                    <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">License</dt>
+                                    <dd className="text-xs text-gray-50 dark:text-gray-300">{repo?.license?.name}</dd>
                                 </div>
                             ) : null}
                             
-                            <div class="flex flex-col-reverse ml-3 sm:ml-6">
-                                <dt class="text-sm font-medium text-gray-600 dark:text-gray-300">Issues</dt>
-                                <dd class="text-xs text-gray-50 dark:text-gray-300">{repo?.open_issues_count}</dd>
+                            <div className="flex flex-col-reverse ml-3 sm:ml-6">
+                                <dt className="text-sm font-medium text-gray-600 dark:text-gray-300">Issues</dt>
+                                <dd className="text-xs text-gray-50 dark:text-gray-300">{repo?.open_issues_count}</dd>
                             </div>
                         </dl>
                     </p>
